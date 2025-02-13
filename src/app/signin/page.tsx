d0200a18b9657ea570a07993082524f262811a2f
Jonathan Ramses <abjonathan@hotmail.com>
2025-02-13 15:36:32 -0300

    feat: ajuste

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -57,14 +57,6 @@ export default function SignIn() {
             <div className="px-26 py-17.5 text-center">
               <Link className="mb-5.5 inline-block" href="/">
                 <Image
-                  className="hidden dark:block"
-                  src={"/images/logo/logo.svg"}
-                  alt="Logo"
-                  width={176}
-                  height={32}
-                />
-                <Image
-                  className="dark:hidden"
                   src={"/images/logo/logo.png"}
                   alt="Logo"
                   width={176}
